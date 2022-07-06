492fe99d8958ae90e764ceb04d41db58fd21462e
Son Do <dominhson580@gmail.com>
2022-07-06 10:11:02 +0700

    Create a function to produce random intergers

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 let playerScores = 0;
 let computerScores = 0;
 let rounds = 1;
+
+// This function returns a random integer between "min" and "max" (inclusive)
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);    
+}
